59c6499f4b8f8cc17a103691c3478d475f9bbc7e
Bharath Vijay SS <Bharath.Vijay@enate.net>
2024-09-18 12:17:36 +0530

    Added Unit Test for QF Modal Component

--- apps/workmanager/src/app/header/preview/quick-find/qf-modal/qf-modal.component.spec.ts
+++ apps/workmanager/src/app/header/preview/quick-find/qf-modal/qf-modal.component.spec.ts
@@ -1,20 +1,89 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { QFModalComponent } from './qf-modal.component'
-// describe('IntelligentSearchContainerComponent', () => {
-//   let component: QFModalComponent;
-//   let fixture: ComponentFixture<QFModalComponent>;
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { QFModalComponent } from './qf-modal.component'
+import { Component } from '@angular/core';
+import { QfSearchBoxComponent } from '../qf-search-box/qf-search-box.component';
+import { QFModalFiltersComponent } from '../qf-modal-filters/qf-modal-filters.component';
+import { QuickFindModalSearchResultsComponent } from '../qf-modal-search-results/qf-modal-search-results.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [QFModalComponent],
-//     }).compileComponents();
+describe('QFModalComponent', () => {
 
-//     fixture = TestBed.createComponent(QFModalComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  @Component({
+    selector: 'en8-qf-search-box',
+    standalone: true,
+    template: ''
+  })
+  class MockQfSearchBoxComponent {
+  }
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  @Component({
+    selector: 'en8-qf-modal-filters',
+    standalone: true,
+    template: ''
+  })
+  class MockQFModalFiltersComponent {
+  }
+
+  @Component({
+    selector: 'en8-qf-modal-search-results',
+    standalone: true,
+    template: ''
+  })
+  class MockQuickFindModalSearchResultsComponent {
+  }
+
+  async function setup() {
+
+    await TestBed.configureTestingModule({
+      imports: [QFModalComponent],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        }
+      ]
+    })
+      .overrideComponent(QFModalComponent, {
+        add: {
+          imports: [
+            MockQfSearchBoxComponent,
+            MockQFModalFiltersComponent,
+            MockQuickFindModalSearchResultsComponent
+          ]
+        },
+        remove: {
+          imports: [
+            QfSearchBoxComponent,
+            QFModalFiltersComponent,
+            QuickFindModalSearchResultsComponent
+          ]
+        }
+      })
+      .compileComponents();
+
+    const fixture: ComponentFixture<QFModalComponent> = TestBed.createComponent(QFModalComponent);
+    const component: QFModalComponent = fixture.componentInstance;
+
+    return {
+      fixture,
+      component
+    }
+  }
+
+  describe('constructor()', () => {
+
+    it('should create instance', async () => {
+      const { component } = await setup();
+
+      expect(component).toBeTruthy();
+    });
+
+    it('should define data property', async () => {
+      const { component } = await setup();
+
+      expect(component.data).toBeTruthy();
+    });
+
+  })
+
+});
