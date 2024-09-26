d4391a1d8f63bfe805981c91bb42b99f3efab16a
Bharath Vijay SS <Bharath.Vijay@enate.net>
2024-09-26 13:54:36 +0530

    Unit Test for QF Modal Filters Component

--- apps/workmanager/src/app/header/preview/quick-find/qf-modal-filters/qf-modal-filters.component.spec.ts
+++ apps/workmanager/src/app/header/preview/quick-find/qf-modal-filters/qf-modal-filters.component.spec.ts
@@ -1,25 +1,75 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { QFModalFiltersComponent } from './qf-modal-filters.component'
+import { QfCheckboxFiltersComponent } from './qf-checkbox-filters/qf-checkbox-filters.component';
+import { QfDateFiltersComponent } from './qf-date-filters/qf-date-filters.component';
+import { Component } from '@angular/core';
 
-describe('IntelligentSearchFiltersComponent', () => {
-  let component: QFModalFiltersComponent;
-  let fixture: ComponentFixture<QFModalFiltersComponent>;
+describe('QFModalFiltersComponent', () => {
 
-  beforeEach(async () => {
+  async function setup() {
+
+    const locale = {
+      key: "value"
+    }
+
+    @Component({
+      selector: 'en8-qf-checkbox-filters',
+      template: '',
+      standalone: true
+    })
+    class MockQfCheckboxFiltersComponent {}
+
+    @Component({
+      selector: 'en8-qf-date-filters',
+      template: '',
+      standalone: true
+    })
+    class MockQfDateFiltersComponent {}
+
     await TestBed.configureTestingModule({
-      imports: [QFModalFiltersComponent],
-    }).compileComponents();
+      imports: [QFModalFiltersComponent]
+    })
+      .overrideComponent(QFModalFiltersComponent, {
+        remove: {
+          imports: [
+            QfCheckboxFiltersComponent,
+            QfDateFiltersComponent,
+          ]
+        },
+        add: {
+          imports: [
+            MockQfCheckboxFiltersComponent,
+            MockQfDateFiltersComponent
+          ]
+        }
+      })
+      .compileComponents();
 
-    fixture = TestBed.createComponent(QFModalFiltersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    const fixture: ComponentFixture<QFModalFiltersComponent> = TestBed.createComponent(QFModalFiltersComponent);
+    const component: QFModalFiltersComponent = fixture.componentInstance;
+
+    fixture.componentRef.setInput('locale', locale);
+
+    return {
+      fixture,
+      component
+    }
+  }
 
   describe('constructor()', () => {
 
-    it('should create instance', () => {
-      expect(component).toBeTruthy();
+    it('should create instance', async () => {
+      const { component } = await setup();
+
+      expect(component).toBeDefined();
     });
 
+    it('locale should be defined', async () => {
+      const { component } = await setup();
+
+      expect(component.locale).toBeDefined();
+    })
+
   })
+
 });
